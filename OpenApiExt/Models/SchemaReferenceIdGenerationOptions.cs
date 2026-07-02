746407f32928c06088df669d08b24bc0258ec59b
stdmyst <nrchvl@gmail.com>
2026-07-02 16:18:29 +0300

    Added schema reference id generation options model.

---
+++ OpenApiExt/Models/SchemaReferenceIdGenerationOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace OpenApiExt.Models;
+
+public record SchemaReferenceIdGenerationOptions
+{
+    public bool UseInlineEnums { get; init; }
+        
+    public bool UseFullTypeNames { get; init; }
+}
